bfad94e790cdc681f28cfb6542ef4979d2bd434a
Md. Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2021-02-08 12:23:21 +0600

    3rd

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,5 @@
-var firebaseConfig = {
+
+  var firebaseConfig = {
     apiKey: "AIzaSyA2ejW0Uv1cA5l3fHHxl0xAChbPh3Q6AdQ",
     authDomain: "waterquality-9daf8.firebaseapp.com",
     databaseURL: "https://waterquality-9daf8.firebaseio.com",
@@ -7,6 +8,7 @@ var firebaseConfig = {
     messagingSenderId: "368707572548",
     appId: "1:368707572548:web:1b06423e74c5f9039bbd11",
     measurementId: "G-2RF3FR06JQ"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
