dd3b42860a46a610e1dbdee4d7b39bbb58ee6974
Md. Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2021-02-08 14:21:38 +0600

    18th

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,15 @@
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyA2ejW0Uv1cA5l3fHHxl0xAChbPh3Q6AdQ",
-    authDomain: "waterquality-9daf8.firebaseapp.com",
-    databaseURL: "https://waterquality-9daf8.firebaseio.com",
-    projectId: "waterquality-9daf8",
-    storageBucket: "waterquality-9daf8.appspot.com",
-    messagingSenderId: "368707572548",
-    appId: "1:368707572548:web:1b06423e74c5f9039bbd11",
-    measurementId: "G-2RF3FR06JQ"
+    apiKey: "AIzaSyBmP-m04bgbm3A1MhprTCvs-U_8CFxhAkc",
+    authDomain: "iot-juel.firebaseapp.com",
+    databaseURL: "https://iot-juel-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "iot-juel",
+    storageBucket: "iot-juel.appspot.com",
+    messagingSenderId: "740208477017",
+    appId: "1:740208477017:web:ca024f022484707f9dc248",
+    measurementId: "G-PRDY29CY81"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
