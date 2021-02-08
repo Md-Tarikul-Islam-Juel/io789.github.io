0521995563032695460704cc3333f583b500ee7c
Md. Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2021-02-08 12:29:50 +0600

    5th

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,4 @@
 
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyBwaadxlqM3zK_BKGyscH3oWIvncsyoFKA",
     authDomain: "cotton-7472f.firebaseapp.com",
