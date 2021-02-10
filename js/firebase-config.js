13566271a9a6a2a689902f8a8bce39704cb1089c
Md. Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2021-02-10 10:39:36 +0600

    fesdf

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBwaadxlqM3zK_BKGyscH3oWIvncsyoFKA",
-    authDomain: "cotton-7472f.firebaseapp.com",
-    databaseURL: "https://cotton-7472f-default-rtdb.firebaseio.com",
-    projectId: "cotton-7472f",
-    storageBucket: "cotton-7472f.appspot.com",
-    messagingSenderId: "925221953729",
-    appId: "1:925221953729:web:b4e08bb1592a5aa7903440",
-    measurementId: "G-8YEVXB2F32"
+    apiKey: "AIzaSyBl9qvj_IKh79ijxDhFAeCWp1uopF-11nY",
+    authDomain: "machine-test-111.firebaseapp.com",
+    databaseURL: "https://machine-test-111-default-rtdb.firebaseio.com",
+    projectId: "machine-test-111",
+    storageBucket: "machine-test-111.appspot.com",
+    messagingSenderId: "853430379245",
+    appId: "1:853430379245:web:bb3c711eb4fb918fc9a2e4",
+    measurementId: "G-7YLJXEZSWQ"
   };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
